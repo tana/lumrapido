91480495749f321af7a6e1585e4289bb4c96a1ed
Satoshi Tanaka <overthere555@gmail.com>
2021-09-24 23:35:41 +0900

    More high-poly sphere

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,19 +1,54 @@
 #include "utils.h"
 
+#include <cmath>
 #include <vsg/maths/transform.h>
 
 vsg::ref_ptr<vsg::Node> createSphere(vsg::vec3 center, float radius)
 {
-  auto builder = vsg::Builder::create();
+  int numTheta = 32;
+  int numPhi = 64;
 
-  vsg::GeometryInfo geomInfo;
-  geomInfo.position = center;
-  geomInfo.dx = vsg::vec3(2.0f * radius, 0.0f, 0.0f);
-  geomInfo.dy = vsg::vec3(0.0f, 2.0f * radius, 0.0f);
-  geomInfo.dz = vsg::vec3(0.0f, 0.0f, 2.0f * radius);
-  geomInfo.color = vsg::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+  int numVertices = (numTheta + 1) * numPhi;
+  int numIndices = 6 * numTheta * numPhi;
 
-  return builder->createSphere(geomInfo);
+  auto vertices = vsg::vec3Array::create(numVertices);
+  auto normals = vsg::vec3Array::create(numVertices);
+  auto texCoords = vsg::vec2Array::create(numVertices);
+  auto indices = vsg::ushortArray::create(numIndices);
+
+  for (int i = 0; i <= numTheta; ++i) {
+    float v = float(i) / numTheta;
+    float theta = vsg::PIf * v;
+
+    for (int j = 0; j < numPhi; ++j) {
+      float u = float(j) / numPhi;
+      float phi = 2 * vsg::PIf * u;
+
+      auto normal = vsg::vec3(sin(theta) * cos(phi), sin(theta) * sin(phi), cos(theta));
+      
+      int vertId = numPhi * i + j;
+      vertices->set(vertId, normal * radius + center);
+      normals->set(vertId, normal);
+      texCoords->set(vertId, vsg::vec2(u, v));
+
+      if (i < numTheta) {
+        int nextI = i + 1;
+        int nextJ = (j + 1) % numPhi;
+        indices->set(6 * vertId, numPhi * i + j);
+        indices->set(6 * vertId + 1, numPhi * nextI + j);
+        indices->set(6 * vertId + 2, numPhi * nextI + nextJ);
+        indices->set(6 * vertId + 3, numPhi * i + j);
+        indices->set(6 * vertId + 4, numPhi * nextI + nextJ);
+        indices->set(6 * vertId + 5, numPhi * i + nextJ);
+      }
+    }
+  }
+
+  auto geom = vsg::Geometry::create();
+  geom->arrays = vsg::DataList{ vertices, normals, texCoords };
+  geom->indices = indices;
+
+  return geom;
 }
 
 vsg::ref_ptr<vsg::Node> createQuad(vsg::vec3 center, vsg::vec3 normal, vsg::vec3 up, float width, float height)
